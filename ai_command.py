ae8d7a6c6808cf308d700136543e91164255d8b0
tattwamasii2p <hypnosis.i2p@gmail.com>
2026-03-14 10:52:23 +0800

    wip

--- ai_command.py
+++ ai_command.py
@@ -48,8 +48,8 @@ class AiCommandHandler:
         self.prompt_last_modified = 0
 
         # Initialize WebSocket client
-        ws_config = self.config.get("websocket", {})
-        self.ws_client = ThreadSafeWebSocketClient(ws_config, response_callback)
+        ws_config = self.config.get("websocket", {"url":"ws://localhost:8443/"})
+        self.ws_client = ThreadSafeWebSocketClient(ws_config["url"], response_callback)
 
         self._lock = Lock()
         self._pending_responses: dict = {}  # request_id -> callback info
