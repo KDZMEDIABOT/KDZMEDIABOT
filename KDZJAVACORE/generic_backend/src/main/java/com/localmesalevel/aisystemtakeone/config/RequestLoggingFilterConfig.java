6d868a4e661ab62de924ed8c57b9260b71bf35a0
tattwamasii2p <hypnosis.i2p@gmail.com>
2026-03-14 22:52:54 +0800

    wip

---
+++ KDZJAVACORE/generic_backend/src/main/java/com/localmesalevel/aisystemtakeone/config/RequestLoggingFilterConfig.java
@@ -0,0 +1,18 @@
+package com.localmesalevel.aisystemtakeone.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeClientInfo(true);
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		return filter;
+	}
+}
